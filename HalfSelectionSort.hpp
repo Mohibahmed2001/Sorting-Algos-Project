92d00e9af5b05f62df92cb10c5ad9b5649dc5218
Mohib Ahmed <113938582+Mohibahmed2001@users.noreply.github.com>
2023-12-13 18:19:24 -0500

    Add files via upload

--- HalfSelectionSort.hpp
+++ HalfSelectionSort.hpp
@@ -1,44 +1,36 @@
 #ifndef HALFSELECTIONSORT_HPP
 #define HALFSELECTIONSORT_HPP
-using namespace std;
+
 #include <iostream>
 #include <vector>
 #include <chrono>
 
 int halfSelectionSort(std::vector<int>& nums, int& duration) {
-   auto start = std::chrono::high_resolution_clock::now();
-  if (nums.size() > 50000) {
-    std::cout << "List size exceeds 50,000. This function cannot handle large lists." << std::endl;
-    return -1;
-  }
-if(nums.size()%2!=0){
-  for (int i = 0; i < nums.size() / 2; ++i) {
-    int min_index = i;
-    for (int j = i + 1; j < nums.size(); ++j) {
-      if (nums[j] < nums[min_index]) {
-        min_index = j;
-      }
+    if (nums.size() > 50000) {
+        std::cout << "Size to big nered" << std::endl;
+        return -1; 
     }
-    std::swap(nums[i], nums[min_index]);
-  }
-auto end = std::chrono::high_resolution_clock::now();
-  duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
-  return nums[(nums.size()/2)];
-}else{
-    for (int i = 0; i < ((nums.size() / 2)-1); ++i) {
-    int min_index = i;
-    for (int j = i + 1; j < nums.size(); ++j) {
-      if (nums[j] < nums[min_index]) {
-        min_index = j;
-      }
+    auto start = std::chrono::high_resolution_clock::now();
+    for (size_t i = 0; i < (nums.size() / 2); ++i) {
+        size_t minIndex = i;
+        for (size_t j = i + 1; j < nums.size(); ++j) {
+            if (nums[j] < nums[minIndex]) {
+                minIndex = j;
+            }
+        }
+        std::swap(nums[i], nums[minIndex]);
     }
-    std::swap(nums[i], nums[min_index]);
-  }
     auto end = std::chrono::high_resolution_clock::now();
-  duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
-  return nums[(nums.size()/2)-1];
+    duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+    size_t middleIndex = nums.size() / 2;
+
+    // Need to determine if even or odd CRUCIAL STEP
+    if (nums.size() % 2 == 0) {
+        return (nums[middleIndex - 1] + nums[middleIndex]) / 2;
+    } else {
+        // ODD
+        return nums[middleIndex];
+    }
 }
-}
- 
 
 #endif
