5c56853de920ee2bddf8619a2491be14e788eb6f
Mohib Ahmed <113938582+Mohibahmed2001@users.noreply.github.com>
2023-12-13 17:28:49 -0500

    Add files via upload

--- HalfSelectionSort.hpp
+++ HalfSelectionSort.hpp
@@ -11,16 +11,27 @@ int halfSelectionSort(std::vector<int>& nums, int& duration) {
     std::cout << "List size exceeds 50,000. This function cannot handle large lists." << std::endl;
     return -1;
   }
-  for (int i = 0; i < nums.size() / 2; ++i) {
-    int min_index = i;
-    for (int j = i + 1; j < nums.size(); ++j) {
-      if (nums[j] < nums[min_index]) {
-        min_index = j;
-      }
+if(nums.size()%2==0){
+    for (int i = 0; i < (nums.size() / 2)-1; ++i) {
+        int min_index = i;
+        for (int j = i + 1; j < nums.size(); ++j) {
+            if (nums[j] < nums[min_index]) {
+            min_index = j;
+        }
     }
     std::swap(nums[i], nums[min_index]);
-  }
-
+    }
+}else{
+    for (int i = 0; i < (nums.size() / 2); ++i) {
+        int min_index = i;
+        for (int j = i + 1; j < nums.size(); ++j) {
+            if (nums[j] < nums[min_index]) {
+            min_index = j;
+        }
+    }
+    std::swap(nums[i], nums[min_index]);
+    }
+}
   auto end = std::chrono::high_resolution_clock::now();
   duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
     if(nums.size()%2==0){
