e6119c71444f744befdcb4f3844bf19735a507df
Mohib Ahmed <113938582+Mohibahmed2001@users.noreply.github.com>
2023-12-13 19:22:25 -0500

    Add files via upload

--- InPlaceMergeSort.hpp
+++ InPlaceMergeSort.hpp
@@ -3,28 +3,30 @@
 
 #include <vector>
 #include <chrono>
+#include <algorithm>
 
-template <typename loser>
-void recursiveInPlaceMergeSort(loser begin, loser end) {
+template <typename RandomAccessIterator>
+void recursiveInPlaceMergeSort(RandomAccessIterator begin, RandomAccessIterator end) {
     if (end - begin > 1) {
-        loser middle = begin + (end - begin) / 2;
+        RandomAccessIterator middle = begin + (end - begin) / 2;
         recursiveInPlaceMergeSort(begin, middle);
         recursiveInPlaceMergeSort(middle, end);
         std::inplace_merge(begin, middle, end);
     }
 }
 
-int inplacemergeSort(std::vector<int>& nums, int& duration) {
+int inPlaceMergeSort(std::vector<int>& nums, int& duration) {
     auto start = std::chrono::high_resolution_clock::now();
     recursiveInPlaceMergeSort(nums.begin(), nums.end());
     auto end = std::chrono::high_resolution_clock::now();
     duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
-    if(nums.size()%2==0){
-        return nums[(nums.size() / 2)-1];
-    }
-    else{
-        return nums[(nums.size()/2)];
+    if (nums.size() % 2 == 0) {
+        return nums[(nums.size() / 2) - 1];
+    } else {
+        return nums[nums.size() / 2];
     }
 }
+
 template void recursiveInPlaceMergeSort(std::vector<int>::iterator begin, std::vector<int>::iterator end);
+
 #endif
